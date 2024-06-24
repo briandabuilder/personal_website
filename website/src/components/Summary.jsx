1b0de7556ad19c031156c24c59b7b81d9bd6453e
briandabuilder <48422595+briandabuilder@users.noreply.github.com>
2024-06-23 19:39:59 -0700

    Summary section complete

--- website/src/components/Summary.jsx
+++ website/src/components/Summary.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 function Summary() {
     return (
         <div>
-            
+            <div className="summary-section">
+                <h1>Hi there!</h1>
+                <div className="summary-text">
+                    My name is Brian and I’m an aspiring software engineer 
+                    and matcha lover with 4 years of experience coding in 
+                    C++, Python, and JavaScript. I love creating websites, 
+                    learning new machine learning algorithms, and building 
+                    quality software applications. I’m driven to leverage 
+                    my skills to make the world more user-friendly and 
+                    interconnected.
+                </div>   
+            </div>
         </div>
     )
 }
