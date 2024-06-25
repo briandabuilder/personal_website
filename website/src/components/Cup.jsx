780cf3f5df0ccf3b42b1fb0453585c22b67732d7
briandabuilder <48422595+briandabuilder@users.noreply.github.com>
2024-06-24 22:47:05 -0700

    boba cup and pearl animations

--- website/src/components/Cup.jsx
+++ website/src/components/Cup.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import BobaCup from '../images/cup.PNG';
+import BobaPearl from '../images/boba.PNG';
 
 function Cup() {
     return (
         <div>
-            
+            <img className="boba-cup" src={BobaCup} alt="boba-cup" />
+            <div className="boba-section">
+                <img className="boba-pearl pearl1" src={BobaPearl} alt="boba-pearl" />
+                <img className="boba-pearl pearl2" src={BobaPearl} alt="boba-pearl" />
+                <img className="boba-pearl pearl3" src={BobaPearl} alt="boba-pearl" />
+                <br />
+                <img className="boba-pearl pearl4" src={BobaPearl} alt="boba-pearl" />
+                <img className="boba-pearl pearl5" src={BobaPearl} alt="boba-pearl" />
+                <img className="boba-pearl pearl6" src={BobaPearl} alt="boba-pearl" />
+            </div>
         </div>
     )
 }
